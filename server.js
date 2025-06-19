87233dfdac626c2164267ccb5f36ec488d165e94
NotSOgenius69 <tanjimhasan35@gmail.com>
2025-06-19 22:41:38 +0600

    Backend-1.0(few updates before deploy)

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ const app = express();
 const port=process.env.PORT || 5000;
 
 //Middlewares
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true
+}
+));
 app.use(express.json());
 app.use('/auth',authRoutes);
 
